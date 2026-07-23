5115e34015d9c19251c323193ec1999f0e98949f
AmitBuilds79 <roastmass79@gmail.com>
2026-07-23 21:48:30 +0530

    Connect Aiven MySQL

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
+import os
 import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="elitemart"
+        host=os.environ.get("DB_HOST"),
+        port=int(os.environ.get("DB_PORT")),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME")
     )
