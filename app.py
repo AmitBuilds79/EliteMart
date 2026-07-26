28ddfbccc9cdffdd8afc306b19b772c9a8a13e73
AmitBuilds79 <roastmass79@gmail.com>
2026-07-26 22:04:22 +0530

    Changed Flask-Mail to SSL test

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.secret_key = "elite123"
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = 'roastmass79@gmail.com'
